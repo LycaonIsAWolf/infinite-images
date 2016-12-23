9eb0cc02f6453c7d47ef403bb4f30f4467741735
LycaonIsAWolf <lycaonsgames@gmail.com>
2016-12-23 01:21:30 -0500

    added image uploading and some styling

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-bodyParser = require('body-parser');
+var fs = require('fs')
+var multer = require('multer');
+var bodyParser = require('body-parser');
 
 var db = require('./controllers/db.js');
 var index = require('./routes/index.js');
@@ -14,8 +16,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', index);
 
-
 db.initialize();
+
+fs.readdir('./static/uploads/', function(err, files){
+	for(var i = 0; i < files.length; i++){
+		console.log(files[i]);
+		fs.unlink('./static/uploads/' + files[i], function(err){});
+	}
+});
 
 var server = app.listen(process.env.PORT || 5000, function(){
 	var host = server.address().address;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var db = require('../controllers/db.js');
 var Post = require('../controllers/post.js');
+var path = require('path');
+
+var multer = require('multer');
+var upload = multer({
+		fileFilter: function(req, file, cb){
+			if((file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif" || file.mimetype=="image/*")){
+				console.log("file ok");
+				cb(null, true);
+			}
+			else{
+				console.log("file rejected");
+				cb(null, false);
+			}
+		},
+
+		storage: multer.diskStorage({
+			destination: function(req, file, cb){
+				cb(null, './static/uploads/');
+			},
+			filename: function(req, file, cb){
+				cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+			}
+		})
+	});
 
 var router = express.Router();
 
 router.get('/', function(req, res){
 	db.get_posts(function(rows, err){
 		if(!err){
-			res.render('index', {posts: rows});
+			res.render('index', {posts: rows.reverse()});
 		}
 		else{
 			console.error(err);
@@ -15,11 +39,9 @@ router.get('/', function(req, res){
 	})
 });
 
-router.post('/post', function(req, res){
-	console.log("post request: " + req.body.body);
-	var post = new Post(req.body.body, "test image");
-	console.log(post.time);
-
+router.post('/post', upload.single('image'), function(req, res){
+	
+	var post = new Post(req.body.body, req.file != undefined ? path.basename(req.file.path) : "");
 	db.add_post(post, function(err){
 		if(err){
 			console.error(err);
@@ -28,6 +50,7 @@ router.post('/post', function(req, res){
 			res.redirect('/post/' + post.id);
 		}
 	});
+
 });
 
 router.param('id', function(req, res, next, id){
