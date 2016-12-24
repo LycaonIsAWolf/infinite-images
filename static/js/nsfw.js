027f57a29ddac208f1a603b465e4ba6dd3b35a15
LycaonIsAWolf <lycaonsgames@gmail.com>
2016-12-24 02:23:13 -0500

    added lightbox

--- static/js/nsfw.js
+++ static/js/nsfw.js
@@ -1,3 +1,6 @@
 $("#nsfw-image").click(function(){
 	$(this).attr("src", $(this).attr("hiding"));
+	$(this).attr("id", "revealed-nsfw");
+	$(this).unbind();
+	$(this).wrap('<a href="' + $(this).attr("src") + '" data-toggle="lightbox"></a>');
 });
